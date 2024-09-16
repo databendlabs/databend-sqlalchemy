63c6ab3b7f052cfb965a3d01d5d6ec10ee566680
Pat Buxton <patrick.buxton@tartansolutions.com>
2024-09-16 10:48:55 +0100

    Try setting format_null_as_str=0
     * This is ugly, should be set global on the database or connection string

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 from sqlalchemy.dialects import registry
+from sqlalchemy import event, Engine, text
 import pytest
 
 registry.register("databend.databend", "databend_sqlalchemy.databend_dialect", "DatabendDialect")
@@ -7,3 +8,10 @@ registry.register("databend", "databend_sqlalchemy.databend_dialect", "DatabendD
 pytest.register_assert_rewrite("sa.testing.assertions")
 
 from sqlalchemy.testing.plugin.pytestplugin import *
+
+
+@event.listens_for(Engine, "connect")
+def receive_engine_connect(conn, r):
+    cur = conn.cursor()
+    cur.execute('SET global format_null_as_str = 0')
+    cur.close()
